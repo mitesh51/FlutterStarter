1c478671a938fae77b6a878373970aaa31469e3b
mitesh51 <mitesh.soni83@gmail.com>
2020-07-11 12:21:00 +0530

    Update news_api_provider_test.dart

--- test/src/api_demo/resources/news_api_provider_test.dart
+++ test/src/api_demo/resources/news_api_provider_test.dart
@@ -7,20 +7,17 @@ import 'package:http/testing.dart';
 void main() {
   final provider = NewsApiProvider();
 
-  test('Sample test', () {
+test('Sample test add', () {
     // SETUP
     final sum = 1 + 3;
     // EXPECTATION
     expect(sum, 4);
   });
-
-  test('Fetch ids from the api call', () async {
-    final mockClient = MockClient((request) async {
-      return Response(json.encode([1, 2, 3, 4]), 200);
-    });
-    provider.client = mockClient;
-    final ids = await provider.fetchTopIds();
-    expect(ids, [1, 2, 3, 4]);
+  test('Sample test multiply', () {
+    // SETUP
+    final sum = 1 * 3;
+    // EXPECTATION
+    expect(sum, 3);
   });
 
   test('Fetch item returns a item model', () async {
