c8252e749a2b07cf9feb31deb6288c0d55321851
mitesh51 <mitesh.soni83@gmail.com>
2020-07-11 12:48:06 +0530

    Update news_api_provider_test.dart

--- test/src/api_demo/resources/news_api_provider_test.dart
+++ test/src/api_demo/resources/news_api_provider_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_app/src/api_demo/resourses/news_api_provider.dart';
+  import 'package:flutter_app/src/api_demo/resourses/news_api_provider.dart';
 import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';
@@ -7,18 +7,13 @@ import 'package:http/testing.dart';
 void main() {
   final provider = NewsApiProvider();
 
-  test('Sample test add', () {
+  test('Sample test', () {
     // SETUP
     final sum = 1 + 3;
     // EXPECTATION
     expect(sum, 4);
   });
-  test('Sample test multiply', () {
-    // SETUP
-    final sum = 1 * 3;
-    // EXPECTATION
-    expect(sum, 3);
-  });
+
   test('Fetch ids from the api call', () async {
     final mockClient = MockClient((request) async {
       return Response(json.encode([1, 2, 3, 4]), 200);
@@ -28,7 +23,15 @@ void main() {
     expect(ids, [1, 2, 3, 4]);
   });
 
-
+  test('Fetch item returns a item model', () async {
+    final mockClient = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+    provider.client = mockClient;
+    final item = await provider.fetchItem(1);
+    expect(item.id, 123);
+  });
 
 
 
